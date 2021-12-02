e1e46aa915be47f1c902b2890fb6c90bc99e5134
Marcus <marmathi@student.chalmers.se>
2021-12-02 14:17:53 +0100

    Day 2 - part 2 finished

--- day2/aoc.py
+++ day2/aoc.py
@@ -3,20 +3,26 @@ input_file = open('day2\input2.txt')
 def forward(x, c):
     print(f'Forward {x} steps')
     c[0] += x
+    c[1] += c[2]*x
+    print(f'x: {c[0]}, y: {c[1]}, aim: {c[2]}')
     return c
 
 def up(y, c):
     print(f'Up {y} steps')
-    c[1] -= y
+    #c[1] -= y
+    c[2] -= y
+    print(f'x: {c[0]}, y: {c[1]}, aim: {c[2]}')
     return c
 
 def down(y, c):
     print(f'Down {y} steps')
-    c[1] += y
+    #c[1] += y
+    c[2] += y
+    print(f'x: {c[0]}, y: {c[1]}, aim: {c[2]}')
     return c
 
 def main():
-    coords = [0,0]
+    coords = [0,0,0]
     for x in input_file.readlines():
         x = x.split(" ")
         coords = eval(f'{x[0]}({x[1]},{coords})')
